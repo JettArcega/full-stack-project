4d64c0ffc8491bc9b6d608d1273b35bf5e9299ac
jett arcega <jarcega31@academic.rrc.ca>
2018-10-25 20:02:46 -0500

    updated the associations

--- warframe/app/models/enemy.rb
+++ warframe/app/models/enemy.rb
@@ -1,3 +1,6 @@
 class Enemy < ApplicationRecord
+    has_many :enemy_items
+    has_many :items, through: :enemy_items
+
     validates :name, presence: true
 end
--- warframe/app/models/item.rb
+++ warframe/app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+    has_many :enemy_items
+    has_many :enemies, through: :enemy_items
+
     validates :name, :description, :price, presence: true
     validates :price, numericality: true
 end
