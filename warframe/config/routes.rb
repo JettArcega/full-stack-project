1dacfafd692e65009a13d6039a6259931cfb3023
jett arcega <jarcega31@academic.rrc.ca>
2018-11-01 14:16:46 -0500

    fixes the routes

--- warframe/config/routes.rb
+++ warframe/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'about' => 'items#about'
+  resources :items, only: [:index, :action]
   get 'items/', to: 'items#index' 
-  get 'items/:id', to: 'items#action', as: 'items_info', id: /\d+/
   root to: 'items#index'
+  get 'about' => 'items#about'
+  get 'items/:id', to: 'items#action', as: 'items_info', id: /\d+/
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
